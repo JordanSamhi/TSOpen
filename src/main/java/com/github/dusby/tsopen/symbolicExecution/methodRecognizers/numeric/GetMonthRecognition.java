6405e15a2e0bb0b3677914ac3e854381f2734324
dusby <j.samhi@me.com>
2019-06-24 14:31:55 +0200

    Add: getMinutes, getMonth and getSeconds recognition

---
+++ src/main/java/com/github/dusby/tsopen/symbolicExecution/methodRecognizers/numeric/GetMonthRecognition.java
@@ -0,0 +1,33 @@
+package com.github.dusby.tsopen.symbolicExecution.methodRecognizers.numeric;
+
+import java.util.List;
+
+import com.github.dusby.tsopen.symbolicExecution.SymbolicExecution;
+import com.github.dusby.tsopen.symbolicExecution.symbolicValues.SymbolicValue;
+import com.github.dusby.tsopen.utils.Constants;
+
+import soot.SootClass;
+import soot.SootMethod;
+import soot.Value;
+import soot.tagkit.StringConstantValueTag;
+
+public class GetMonthRecognition extends NumericMethodsRecognitionHandler {
+
+	public GetMonthRecognition(NumericMethodsRecognitionHandler next, SymbolicExecution se) {
+		super(next, se);
+	}
+
+	@Override
+	public boolean processLongMethod(SootMethod method, Value base, List<Value> args, SymbolicValue sv) {
+		SootClass declaringClass = method.getDeclaringClass();
+		String methodName = method.getName();
+		if(this.containsTag(base, Constants.NOW_TAG)) {
+			if(declaringClass.getName().equals(Constants.JAVA_UTIL_DATE) && methodName.equals(Constants.GET_MONTH)) {
+				sv.addTag(new StringConstantValueTag(Constants.MONTH_TAG));
+				return true;
+			}
+		}
+		return false;
+	}
+
+}

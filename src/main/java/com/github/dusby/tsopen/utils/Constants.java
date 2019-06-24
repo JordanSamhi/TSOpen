f8ea1039f5f7efd965bc52aa6040186381f50b58
dusby <j.samhi@me.com>
2019-06-24 14:11:31 +0200

    Add: constants class

---
+++ src/main/java/com/github/dusby/tsopen/utils/Constants.java
@@ -0,0 +1,42 @@
+package com.github.dusby.tsopen.utils;
+
+public class Constants {
+	public static final String UNKNOWN_STRING = "UNKNOWN_STRING";
+	public static final String EMPTY_STRING = "";
+	public static final String GET_INSTANCE = "getInstance";
+	public static final String NOW = "now";
+	public static final String GET_LAST_KNOW_LOCATION = "getLastKnownLocation";
+	public static final String GET_LAST_LOCATION = "getLastLocation";
+	public static final String CREATE_FROM_PDU = "createFromPdu";
+	public static final String GET_LONGITUDE = "getLongitude";
+	public static final String GET_LATITUDE = "getLatitude";
+	public static final String CURRENT_TIME_MILLIS = "currentTimeMillis";
+	public static final String GET_MINUTES = "getMinutes";
+	public static final String GET_SECONDS = "getSeconds";
+	public static final String GET_MONTH = "getMonth";
+
+	public static final String NOW_TAG = "#now";
+	public static final String HERE_TAG = "#here";
+	public static final String SMS_TAG = "#sms";
+	public static final String LONGITUDE_TAG = "#here/#longitude";
+	public static final String LATITUDE_TAG = "#here/#latitude";
+	public static final String SECONDS_TAG = "#now/#seconds";
+	public static final String MINUTES_TAG = "#now/#minutes";
+	public static final String MONTH_TAG = "#now/#month";
+
+	public static final String JAVA_UTIL_CALENDAR = "java.util.Calendar";
+	public static final String JAVA_UTIL_DATE = "java.util.Date";
+	public static final String JAVA_UTIL_GREGORIAN_CALENDAR = "java.util.Calendar";
+	public static final String JAVA_TIME_LOCAL_DATE_TIME = "java.time.LocalDateTime";
+	public static final String JAVA_TIME_LOCAL_DATE = "java.time.LocalDate";
+	public static final String JAVA_LANG_STRING = "java.lang.String";
+	public static final String JAVA_LANG_STRING_BUILDER = "java.lang.StringBuilder";
+	public static final String JAVA_LANG_STRING_BUFFER = "java.lang.StringBuffer";
+	public static final String ANDROID_LOCATION_LOCATION = "android.location.Location";
+	public static final String ANDROID_LOCATION_LOCATION_MANAGER = "android.location.LocationManager";
+	public static final String COM_GOOGLE_ANDROID_GMS_LOCATION_LOCATION_RESULT = "com.google.android.gms.location.LocationResult";
+	public static final String ANDROID_TELEPHONY_SMSMESSAGE = "android.telephony.SmsMessage";
+	public static final String JAVA_LANG_SYSTEM = "java.lang.System";
+	public static final String INT = "int";
+	public static final String LONG = "long";
+}

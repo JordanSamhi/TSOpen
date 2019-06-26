b0a2088b070472def4faf024599b927d92da80c4
dusby <j.samhi@me.com>
2019-06-26 11:48:42 +0200

    Fix: propagate all args

--- src/main/java/com/github/dusby/tsopen/symbolicExecution/methodRecognizers/strings/FormatRecognition.java
+++ src/main/java/com/github/dusby/tsopen/symbolicExecution/methodRecognizers/strings/FormatRecognition.java
@@ -20,13 +20,11 @@ public class FormatRecognition extends StringMethodsRecognitionHandler {
 	@Override
 	public List<SymbolicValue> processStringMethod(SootMethod method, Value base, List<Value> args) {
 		List<SymbolicValue> results = new ArrayList<SymbolicValue>();
-		Value effectiveArg = null;
 		if(method.getName().equals(Constants.FORMAT) && base.getType().toString().equals(Constants.JAVA_TEXT_SIMPLE_DATE_FORMAT)) {
 			this.addSimpleResult(base, results);
-			if(args.size() > 0) {
-				effectiveArg = args.get(0);
+			for(Value arg : args) {
 				for(SymbolicValue sv : results) {
-					Utils.propagateTags(effectiveArg, sv, this.se);
+					Utils.propagateTags(arg, sv, this.se);
 				}
 			}
 			return results;

0bf8117e7ce8ce88ad739dc39de54f8cbb337783
dusby <j.samhi@me.com>
2019-06-29 09:15:30 +0200

    Add: binopValue

---
+++ src/main/java/com/github/dusby/tsopen/symbolicExecution/symbolicValues/BinOpValue.java
@@ -0,0 +1,46 @@
+package com.github.dusby.tsopen.symbolicExecution.symbolicValues;
+
+import com.github.dusby.tsopen.symbolicExecution.SymbolicExecution;
+
+import soot.Value;
+
+public class BinOpValue extends AbstractSymbolicValue {
+
+	private Value op1;
+	private Value op2;
+	private String symbol;
+
+	public BinOpValue(SymbolicExecution se, Value op1, Value op2, String symbol) {
+		super(se);
+		this.op1 = op1;
+		this.op2 = op2;
+		this.symbol = symbol;
+		this.getSymbolicValues(this.op1);
+		this.getSymbolicValues(this.op2);
+	}
+
+	@Override
+	public String getValue() {
+		return String.format("%s%s%s", this.computeValue(this.op1), this.symbol, this.computeValue(this.op2));
+	}
+
+	@Override
+	public boolean isSymbolic() {
+		return true;
+	}
+
+	@Override
+	public boolean isConstant() {
+		return false;
+	}
+
+	@Override
+	public boolean isMethodRepresentation() {
+		return false;
+	}
+
+	@Override
+	public boolean isObject() {
+		return false;
+	}
+}
